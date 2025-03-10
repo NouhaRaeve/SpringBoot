7b218d4404fa664e3a867920b614b25135584a7a
Nouha Rouached <101023113+NouhaRaeve@users.noreply.github.com>
2025-03-10 13:15:06 +0100

    Delete demo directory

--- demo/src/main/java/com/example/demo/entity/DonationType.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.entity;
-
-public enum DonationType {
-    MONETARY, BLOOD, IN_KIND;
-}
--- demo/src/main/java/com/example/demo/entity/TypeChambre.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.entity;
-
-public enum TypeChambre {
-    Simple,Double,Triple
-}
